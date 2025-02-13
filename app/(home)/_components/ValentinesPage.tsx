e5e72cac0fe932ab46dd1b9c1f69389b414b170c
strwbry <155338364+chocological13@users.noreply.github.com.>
2025-02-13 13:14:21 +0700

    first commit

---
+++ app/(home)/_components/ValentinesPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QuestionBox from "@/app/(home)/_components/QuestionBox";
+
+const ValentinesPage = () => {
+    return (
+        <div>
+            I'm a silli lil catto
+            <QuestionBox />
+        </div>
+    );
+};
+
+export default ValentinesPage;
---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import ValentinesPage from "@/app/(home)/_components/ValentinesPage";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-100 to-purple-100">
+      <ValentinesPage />
+    </div>
+  );
+}
